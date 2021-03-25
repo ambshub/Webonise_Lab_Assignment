19dc04e0ef6734d3593fd643386793aed1e263ec
shubham_shukla <shubham_shukla@localhost>
2021-03-25 17:17:17 +0530

    My First commit for Webonise lab assignment

---
+++ UserRegistration1/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into User (user_id,user_fname,user_lname,user_email,user_pass,user_mobile) values (1,'Shubham','Shukla','ambshub@yahoo.co.in','test@123','1234567890');
+insert into User (user_id,user_fname,user_lname,user_email,user_pass,user_mobile) values (2,'Yogita','Bajpai','abcd@gmail.com','test@1','0987654321');
+
